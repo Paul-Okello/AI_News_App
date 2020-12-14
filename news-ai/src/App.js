73fb546c2eef78dab2d850859c79f0bd858c7cd7
Paul Okello <paulokello273@gmail.com>
2020-12-14 14:37:32 +0300

    Connect a react app with allan AI  🈯🈯🈯

--- news-ai/src/App.js
+++ news-ai/src/App.js
@@ -1,6 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
+import alanBtn from "@alan-ai/alan-sdk-web";
+
+const alanKey =
+  "7db1211d5ebd703a25bb49633c7c816e2e956eca572e1d8b807a3e2338fdd0dc/stage";
 
 const App = () => {
+  useEffect(() => {
+    alanBtn({
+      key: alanKey,
+      onCommand: ({ command }) => {
+        if (command === "testCommand") {
+          alert("This code was excuted");
+        }
+      },
+    });
+  }, []);
   return <div></div>;
 };
 
