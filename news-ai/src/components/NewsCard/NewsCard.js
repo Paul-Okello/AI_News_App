c6f06c562ac3d3db00b401161317f76b47663158
Paul Okello <paulokello273@gmail.com>
2020-12-14 19:37:20 +0300

    NewsCard

--- news-ai/src/components/NewsCard/NewsCard.js
+++ news-ai/src/components/NewsCard/NewsCard.js
@@ -1,7 +1,48 @@
 import React from "react";
+import {
+  Button,
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Typography,
+} from "@material-ui/core";
 
-const NewsCard = () => {
-  return <div>News Card</div>;
+const NewsCard = ({ article: {}, i }) => {
+  return (
+    <Card>
+      <CardActionArea>
+        <CardMedia />
+        <div>
+          <Typography
+            variant="body2"
+            color="testSecondary"
+            component="h2"
+          ></Typography>
+          <Typography
+            variant="body2"
+            color="testSecondary"
+            component="h2"
+          ></Typography>
+        </div>
+        <Typography gutterBottom variant="h5"></Typography>
+        <CardContent>
+          <Typography
+            variant="body2"
+            color="textSecondary"
+            component="p"
+          ></Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary"></Button>
+        <Typography variant="h5" color="textSecondary">
+          {article.description}
+        </Typography>
+      </CardActions>
+    </Card>
+  );
 };
 
 export default NewsCard;
