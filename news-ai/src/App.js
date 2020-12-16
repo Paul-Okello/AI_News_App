351010fca2b045669490855ad6986f655978d2e9
Paul Okello <paulokello273@gmail.com>
2020-12-16 13:57:18 +0300

    🎈🎈🎈General App Look 🎈🎈🎈

--- news-ai/src/App.js
+++ news-ai/src/App.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from "react";
 import alanBtn from "@alan-ai/alan-sdk-web";
 import NewsCards from "./components/NewsCards/NewsCards";
+import useStyles from "./styles";
 
 const alanKey =
   "7db1211d5ebd703a25bb49633c7c816e2e956eca572e1d8b807a3e2338fdd0dc/stage";
 
 const App = () => {
+  const classes = useStyles();
   const [newsArticles, setNewsArticles] = useState([]);
   useEffect(() => {
     alanBtn({
@@ -19,6 +21,13 @@ const App = () => {
   }, []);
   return (
     <div>
+      <div className={classes.logoContainer}>
+        <img
+          src="https://miro.medium.com/max/1000/1*CLXr0JGZTZTrjPN5JGg5pQ.png"
+          alt="logo"
+          className={classes.alanLogo}
+        />
+      </div>
       <NewsCards articles={newsArticles} />
     </div>
   );
